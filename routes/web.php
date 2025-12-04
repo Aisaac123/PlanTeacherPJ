88f645bdf6386e5dc99b4ba592285f61330e2f22
ISAAC <isaacjacome9@gmail.com>
2025-12-04 16:12:18 -0500

    prueba ruta

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->to(\Filament\Pages\Dashboard::getUrl());
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Laravel is running',
+        'php_version' => PHP_VERSION,
+        'env' => config('app.env')
+    ]);
 })->name('home');
 
 Route::get('/app/logout', function () {
