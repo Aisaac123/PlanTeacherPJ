3bb91f63a756287bbecbaf8df35e5f8a57e59efc
ISAAC <isaacjacome9@gmail.com>
2025-12-03 19:43:03 -0500

    plan de trabajo

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -99,5 +100,10 @@ class User extends Authenticatable
     public function tutorias(): HasMany
     {
         return $this->hasMany(Tutoria::class, 'user_id');
+    }
+
+    public function planTrabajo(): HasOne
+    {
+        return $this->hasOne(PlanTrabajo::class);
     }
 }
